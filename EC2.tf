fac34134628b1784a40e50c208910bba9401fa1d
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-21 11:45:02 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "myInstance" {
+  count         = 4 
   ami           = var.ami_id
   instance_type = var.instance_type
   tags= {
-    Name = var.tag_name
+    Name = var.tag_name-${count.index}
   }
 }
