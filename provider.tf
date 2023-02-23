654869b981fdf4c964720eaf39249f0d03512064
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-23 11:22:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,6 +3,6 @@ provider "aws" {
  #access_key = var.access_key
  #secret_key = var.secret_key
   assume_role {
-    role_arn = var.role
+    role_arn = "arn:aws:iam::937502484942:role/jenkins"
   }
   }
