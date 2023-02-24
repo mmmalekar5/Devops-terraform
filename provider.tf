0026f66585e20deb8c132fae35341bee2e9a9c34
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-24 09:57:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region     = var.region
- access_key = var.access_key
- secret_key = var.secret_key
+ #access_key = var.access_key
+ #secret_key = var.secret_key
   assume_role {
-    role_arn = "arn:aws:iam::937502484942:role/jenkins"
+    role_arn = "arn:aws:iam::937502484942:role/terraform"
   }
   }
