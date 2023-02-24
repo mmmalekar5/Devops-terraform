9a318e6c4743437913ccfd941a8ac24a08c62dda
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-24 09:59:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,5 @@
 provider "aws" {
   region     = var.region
- #access_key = var.access_key
- #secret_key = var.secret_key
-  assume_role {
-    role_arn = "arn:aws:iam::937502484942:role/terraform"
-  }
+  access_key = var.access_key
+  secret_key = var.secret_key
   }
