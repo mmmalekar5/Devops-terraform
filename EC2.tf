7d9d2d7b43b10d76a193c602bc228f32434a5533
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-25 14:52:55 +0530

    Update EC2.tf
    adding multi acc.

--- EC2.tf
+++ EC2.tf
@@ -1,7 +1,17 @@
 resource "aws_instance" "myInstance" {
+  provider      = aws.dev
   ami           = var.ami_id
   instance_type = var.instance_type
   tags= {
     Name = var.tag_name
   }
 }
+resource "aws_instance" "myEC2" {
+  provider      = aws.test
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  tags= {
+    Name = "myEC2"
+  }
+}
+
