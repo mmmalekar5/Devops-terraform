e6f640aa0c1a946d9d79607e5d908daf3265c045
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-25 14:50:18 +0530

    Update provider.tf
    adding multi acc. credentials

--- provider.tf
+++ provider.tf
@@ -1,5 +1,13 @@
 provider "aws" {
+  alias      = "dev"
   region     = var.region
-  #access_key = var.access_key
-  #secret_key = var.secret_key
+  access_key = var.dev_access_key
+  secret_key = var.dev_secret_key
+  }
+
+provider "aws" {
+  alias      = "test"
+  region     = "us-east-1"
+  access_key = var.test_access_key
+  secret_key = var.test_secret_key
   }
