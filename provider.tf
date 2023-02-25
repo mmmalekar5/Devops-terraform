f4a5c7ee3e07d800efe38f4e49ae3e219555adc6
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-25 23:00:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,6 @@
 provider "aws" {
-  alias      = "dev"
   region     = var.region
-  access_key = var.dev_access_key
-  secret_key = var.dev_secret_key
-  }
-
-provider "aws" {
-  alias      = "test"
-  region     = "us-east-1"
-  access_key = var.test_access_key
-  secret_key = var.test_secret_key
-  }
+  profile    = "dev"
+} 
+ 
+  
