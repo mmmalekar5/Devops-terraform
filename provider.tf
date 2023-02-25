4e762a515be9f59f03a50ba50f62e49a6cbc9258
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-25 23:48:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region                   = var.region
-  access_key               = "AKIAYURHTUG4YVS4ASNU"
-  secret_key               = "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"
+  access_key               = "AKIA5UR4AVHHOVEFFWIA"
+  secret_key               = "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"
 } 
  
   
