1859d965f6280166b12250c114b9aa30cd180c6d
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-25 23:01:24 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,17 +1,8 @@
 resource "aws_instance" "myInstance" {
-  provider      = aws.dev
   ami           = var.ami_id
   instance_type = var.instance_type
   tags= {
     Name = var.tag_name
-  }
-}
-resource "aws_instance" "myEC2" {
-  provider      = aws.test
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  tags= {
-    Name = "myEC2"
   }
 }
 
