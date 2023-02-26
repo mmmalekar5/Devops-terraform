cda86043b692d8b35681e053ffad7f90d8193e23
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-26 12:33:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,12 +6,19 @@ terraform {
     }
   }
 }
-provider "aws" {
+#provider "aws" {
  # shared_config_files      = ["/root/.aws/config"]
  # shared_credentials_files = ["/root/.aws/credentials"]
-  region                   = "ap-south-1"
- #profile                  = "test"
-  access_key               = "AKIA5UR4AVHHEDYWWCE4"
-  secret_key               = "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"
-}
+  #region                   = "ap-south-1"
+  #profile                  = "test"
+  #access_key               = "AKIA5UR4AVHHOMWZQ2PO"
+  #secret_key               = "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"
 
+
+provider “aws” {
+profile = “awscross” # local aws credential/config profile name
+assume_role {
+role_arn = “arn:aws:iam::593862304185:role/access-to-jenkins”
+session_name = “terraform”
+}
+}
