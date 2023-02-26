83f9134293a3324d4a96d7d198280a155c53c0a1
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-26 09:04:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,8 +9,8 @@ terraform {
 provider "aws" {
   shared_config_files      = ["/home/ec2-user/.aws/config"]
   shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
- # region                   = var.region
- # profile                  = "dev"
+  region                   = var.region
+  profile                  = var.profile
  # access_key               = "AKIAYURHTUG4RIOGNHSS"
  # secret_key               = "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"
 }
