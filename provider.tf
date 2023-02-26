9e86ee5e3a4ed31de32ffc11bf299ab628fac3e2
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-26 11:33:43 +0530

    Update provider.tf
    provide access key id

--- provider.tf
+++ provider.tf
@@ -10,8 +10,8 @@ provider "aws" {
  # shared_config_files      = ["/root/.aws/config"]
  # shared_credentials_files = ["/root/.aws/credentials"]
   region                   = "ap-south-1"
-  profile                  = "test"
- # access_key               = "AKIAYURHTUG4RIOGNHSS"
- # secretkey               = "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"
+ #profile                  = "test"
+  access_key               = "AKIAYURHTUG4RIOGNHSS"
+  secretkey               = "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"
 }
 
