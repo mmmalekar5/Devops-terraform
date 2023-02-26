c66f1394c0a8db05c8f9ac3b90ef2b5058282155
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-26 15:27:09 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.56.0"
-    }
-  }
-}
 #provider "aws" {
  # shared_config_files      = ["/root/.aws/config"]
  # shared_credentials_files = ["/root/.aws/credentials"]
