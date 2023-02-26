b7cd6a58981c4a93a3c57884ade2fb250925d255
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-26 15:45:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,13 @@
-provider "aws" {
- # shared_config_files      = ["/root/.aws/config"]
- # shared_credentials_files = ["/root/.aws/credentials"]
-  region                   = "ap-south-1"
-  #profile                  = "test"
-  access_key               = "AKIA5UR4AVHHFHF2B6IZ"
-  secret_key               = "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"
-}
 # provider "aws" {
-#   shared_config_files      = ["/home/ec2-user/.aws/config"]     
-#   shared_credentials_files  = ["/home/ec2-user/.aws/credentials"] 
-#   profile = "mayur" # local aws credential/config profile name
+#   region                   = "ap-south-1"
+#   #profile                  = "test"
+#   access_key               = "AKIA5UR4AVHHFHF2B6IZ"
+#   secret_key               = "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"
 # }
+provider "aws" {
+  shared_config_files      = ["/home/ec2-user/.aws/config"]     
+  shared_credentials_files  = ["/home/ec2-user/.aws/credentials"] 
+  profile = "mayur" # local aws credential/config profile name
+}
 
 
