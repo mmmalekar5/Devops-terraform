e7108eab6e160a12cc57dff96d1d88ec1ce47ac7
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-26 08:27:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 provider "aws" {
   region                   = var.region
-  access_key               = "AKIAYURHTUG4RIOGNHSS"
-  secret_key               = "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"
-} 
- 
-  
+  profile                  = "default"
+ # access_key               = "AKIAYURHTUG4RIOGNHSS"
+ # secret_key               = "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"
+}
+
