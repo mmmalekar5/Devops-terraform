53f2817893d6a8c2bf162be7b3b80fc990f85a44
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-27 14:56:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,19 +1,24 @@
-# provider "aws" {
-#   region                   = "ap-south-1"
-#   #profile                  = "test"
-#   access_key               = "AKIA5UR4AVHHFHF2B6IZ"
-#   secret_key               = "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"
-# }
+provider "aws" {
+          region    = var.region
+          profile   = var.profile
+    }
+
+
+
+
+
 # provider "aws" {
 #   shared_config_files      = ["/home/ec2-user/.aws/config"]     
 #   shared_credentials_files  = ["/home/ec2-user/.aws/credentials"] 
 #   profile = "mayur" # local aws credential/config profile name
+#   access_key               = "AKIA5UR4AVHHFHF2B6IZ"
+#   secret_key               = "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"
 # }
 
-provider "aws" {
-  region                = "ap-south-1"
-  assume_role {
-    role_arn     = "arn:aws:iam::937502484942:role/jenkins"   
-            }
-  }
+# provider "aws" {
+#   region                = "ap-south-1"
+#   assume_role {
+#     role_arn     = "arn:aws:iam::937502484942:role/jenkins"   
+#             }
+#   }
 
