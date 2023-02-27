4a10a44cfc010c01e317d1d01f8f1bfe81988702
Mayur_M_Malekar <118094965+mmmalekar5@users.noreply.github.com>
2023-02-27 15:11:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,8 @@
 provider "aws" {
           region    = var.region
           profile   = var.profile
+          shared_config_files       = ["/home/ec2-user/.aws/config"]     
+          shared_credentials_files  = ["/home/ec2-user/.aws/credentials"] 
     }
 
 
